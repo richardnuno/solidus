4e0b64de5f43dd146cf0066df6d2911f0f0560f9
Richard Nuno <me@richardnuno.com>
2015-08-24 10:44:00 -0400

    WIP

--- core/app/models/spree/return_item/exchange_variant_eligibility/same_option_value.rb
+++ core/app/models/spree/return_item/exchange_variant_eligibility/same_option_value.rb
@@ -20,11 +20,15 @@ module Spree
       # blue pants with 34 waist and 32 inseam
 
       def self.eligible_variants(variant)
-        product_variants = SameProduct.eligible_variants(variant).includes(option_values: :option_type)
+        product_variants = SameProduct.eligible_variants(variant)
 
         relevant_option_values = variant.option_values.select { |ov| option_type_restrictions.include? ov.option_type.name }
         if relevant_option_values.present?
-          product_variants.select { |v| (relevant_option_values & v.option_values) == relevant_option_values }
+          variant_ids = Spree::OptionValuesVariant.
+            where(variant_id: product_variants.distinct.pluck(:id)).
+            where(option_value: relevant_option_values).distinct.
+            pluck(:variant_id)
+          product_variants.where(id: variant_ids)
         else
           product_variants
         end
